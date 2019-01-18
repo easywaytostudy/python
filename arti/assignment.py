febfec90bee8867328102d18f37581561b7268dc
gaurav121991@yahoo.com <incomparable1142>
2019-01-18 23:15:57 +0530

    my first commit

---
+++ arti/assignment.py
@@ -0,0 +1,51 @@
+
+	
+	
+s=[]
+a=[]
+a.insert(0,"INDIA")
+a.insert(1,"AFGHANISTAN")
+a.insert(2,"AUSTRALIA")
+a.insert(3,"CANADA")
+a.insert(4,"CHINA")
+a.insert(5,"FRANCE")
+a.insert(6,"IRAN")
+a.insert(7,"IRAQ")
+a.insert(8,"IRELAND")
+a.insert(9,"ITALY")
+
+b=[]
+b.insert(0,"DELHI")
+b.insert(1,"KABUL")
+b.insert(2,"CANBERRA")
+b.insert(3,"OTTAWA")
+b.insert(4,"BEIJING")
+b.insert(5,"PARIS")
+b.insert(6,"TEHRAN")
+b.insert(7,"BAGHDAD")
+b.insert(8,"DUBLIN")
+b.insert(9,"ROME")
+
+
+item = {}
+item[a[0]]=b[0]	
+item[a[1]]=b[1]	
+item[a[2]]=b[2]	
+item[a[3]]=b[3]	
+item[a[4]]=b[4]	
+item[a[5]]=b[5]	
+item[a[6]]=b[6]	
+item[a[7]]=b[7]	
+item[a[8]]=b[8]	
+item[a[9]]=b[9]	
+
+
+"""item['1'] = "India","Delhi"
+item['2'] =  "England","London
+"""
+		
+s.append(item)
+print "List of all countries is",a
+print "Dictionary of Countries &  their Capitals is", s
+
+
